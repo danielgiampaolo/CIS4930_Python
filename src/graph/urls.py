7fbc8ba9dafee54d2b1b4c89b1c1193e30c8074d
Adrian Salazar <adrian.salazar@ufl.edu>
2020-06-24 01:36:46 -0400

    Added templates, began context based-sidebar

--- src/graph/urls.py
+++ src/graph/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('old', views.index, name='index'),
-    path('', views.node_list, name='list'),
+    path('', views.index, name='graph_view'),
 ]
--- src/graph/views.py
+++ src/graph/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
+# third parameter is a map, key = name, value = information
 def index(request):
-    return render(request, 'graph/index.html')
 
-def node_list(request):
-    return render(request, 'graph/node_list.html')
+    # get vertices (or anything) from session info here
+    # then fill "sidebar_context.html"
+
+    graph_data = {
+        "bottom": "text",
+        "nodes": ["a", "b"],
+        "edges": ["a to b"],
+        "type": "Undirected",
+    }
+
+    return render(request, 'index.html', graph_data)
