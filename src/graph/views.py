33373deea49e927e7bb7c7fcc1c35b0c93e5056b
Adrian Salazar <adrian.salazar@ufl.edu>
2020-06-24 16:52:23 -0400

    Starting session based sidebar

--- src/graph/views.py
+++ src/graph/views.py
@@ -1,11 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_http_methods
 
-# third parameter is a map, key = name, value = information
+@require_http_methods(["GET", "POST"]) # now only GET & POST allowed
 def index(request):
 
     # get vertices (or anything) from session info here
     # then fill "sidebar_context.html"
+    
+    test_fill_data = {
+        "nodes": request.session.get('nodes', []),  # need to save elsewhere
+        "edges": request.session.get('edges', []),
+        "type": "Undirected",
+    }
+
+    # this way of accessing a session variable
+    # gives us a default value if it doesnt exist
+    # var = req.session.get('something', default) default being any value
+
+    # delete -> del req.session['something']
 
     graph_data = {
         "bottom": "text",
@@ -13,6 +25,8 @@ def index(request):
         "edges": ["a to b"],
         "type": "Undirected",
     }
+
+
 
     # TODO:
     #  1. See if its possible to modify the things above like a text box
