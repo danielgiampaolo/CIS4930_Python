8df9a7db206b7c665399bc51461ee549f5920e58
Adrian Salazar <adrian.salazar@ufl.edu>
2020-06-23 22:58:48 -0400

    Added Bootstrap
    some old views remaining

--- src/graph/urls.py
+++ src/graph/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('old', views.index, name='index'),
+    path('', views.node_list, name='list'),
 ]
--- src/graph/views.py
+++ src/graph/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import get_object_or_404, render
 
 def index(request):
     return render(request, 'graph/index.html')
+
+def node_list(request):
+    return render(request, 'graph/node_list.html')
