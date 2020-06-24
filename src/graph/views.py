8220af8a88fe6ee04d482689d9c84a88b6ec2fd8
Adrian Salazar <adrian.salazar@ufl.edu>
2020-06-24 10:35:22 -0400

    Loop in HTML base added

--- src/graph/views.py
+++ src/graph/views.py
@@ -9,9 +9,15 @@ def index(request):
 
     graph_data = {
         "bottom": "text",
-        "nodes": ["a", "b"],
+        "nodes": ["test1", "test2"],
         "edges": ["a to b"],
         "type": "Undirected",
     }
+
+    # TODO:
+    #  1. See if its possible to modify the things above like a text box
+    #  2. Add button for update, & get information from "text box" ^^
+    #  3. Add a background color to sidebar, a nice soft color :)
+    #  4. Start looking into how to access sessions
 
     return render(request, 'index.html', graph_data)
