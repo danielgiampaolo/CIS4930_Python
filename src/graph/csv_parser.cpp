cbea187c7501be3c2e0debbbd09514ed98705876
Enzo Egues <enzoegues@yahoo.com>
2020-07-25 20:37:37 -0400

    Forgot to add the parser. Reminder that files here are proabbly broken due to github acting weird in my comp.

--- src/graph/csv_parser.cpp
+++ src/graph/csv_parser.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 #include <cstring>
 #include <set>
-
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -16,8 +15,8 @@ while(std::getline(ss, token, ',')) {
 */
 
 typedef struct {
-  char** nodes;                         
-  char**** edges;                          
+    char** nodes;
+    char*** edges;
 } Ext_Struct;
 
 Ext_Struct PerfRead;
@@ -25,74 +24,145 @@ Ext_Struct PerfRead;
 void init(void)
 {
     // testing
-    PerfRead.nodes = ["t"];
-    PerfRead.edges = [["5","6"]];
+    PerfRead.nodes = nullptr;
+    PerfRead.edges = nullptr;
 }
 
 // nvm
 
 
-
 extern Ext_Struct PerfRead;
-
 extern void read(const char*);
 
-void read(const char* data) 
+void read(const char* data)
 {
-    
-    std::string line;
+    init();
+    vector<char*> words;
+    vector<char**> rows;
+    set<string> node_set;
+    int letter_a_count = 0;
+    int letter_b_count = 0;
+    int letter_c_count = 0;
     int line_count = 0;
     int element_count = 0;
     std::stringstream ss;
-    
+    int index = 0;
+
 
     std::istringstream test_stream(data);
     string node1;
     string node2;
     string weight;
-    char ** row;
 
 
-    /*strcpy brings a compiler error.
-      Instead we have to use strcpy_s(char *restrict dest, rsize_t destsz, const char *restrict src);
-    */
-    /* Test cope with strcpy_s
-    std::string str = "string";
-    char *cstr = new char[str.length() + 1];
-    strcpy_s(cstr,str.length(), str.c_str());
-    // Nope doesnt work
-    delete [] cstr;
-    https://stackoverflow.com/questions/7352099/stdstring-to-char
-    */
-    while (std::getline(test_stream, node1, ',')){
-      std::getline(test_stream, weight, ',');
-      std::getline(test_stream, node2);
-      
-       
+    std::getline(test_stream, node1, ',');
+    std::getline(test_stream, weight, ',');
+    std::getline(test_stream, node2, '\n'); //Clears the Node,Link,and Node header
+
+    while (std::getline(test_stream, node1, ',')) {
+        std::getline(test_stream, weight, ',');
+        std::getline(test_stream, node2, '\n');
+
+        while (node1.at(0) == ' ') // Erases any spaces that may exist
+            node1.erase(0, 1);
+        while (weight.at(0) == ' ')
+            weight.erase(0, 1);
+        while (node2.at(0) == ' ')
+            node2.erase(0, 1);
+
+        letter_a_count += node1.size(); // Idea was to allocate memory for bigger names but that may bee too difficult
+        letter_b_count += weight.size();
+        letter_c_count += node2.size();
+        words.push_back((char*)malloc(25 * sizeof(char))); // As of right now it holds 24 chars plus the null operator
+        words.push_back((char*)malloc(25 * sizeof(char)));
+        words.push_back((char*)malloc(25 * sizeof(char)));
+
+        char* letter_iter_a = words.at(index); //Points the pointers in the word vector to a random char*
+        char* letter_iter_b = words.at(index + 1);
+        char* letter_iter_c = words.at(index + 2);
+
+        node_set.insert(node1); // Adds the node strings to the set
+        node_set.insert(node2);
+
+        for (int i = 0; i < node1.size(); i++) //Manually adds each character to their respective array
+        {
+               
+            *letter_iter_a = node1.at(i);
+             letter_iter_a++;
+            
+        }
+        for (int i = 0; i < weight.size(); i++)
+        {
+
+            *letter_iter_b = weight.at(i);
+            letter_iter_b++;
+        }
+        for (int i = 0; i < node2.size(); i++)
+        {
+                *letter_iter_c = node2.at(i);
+                letter_iter_c++;
+        }
+        *letter_iter_a = '\0';
+        *letter_iter_b = '\0';
+        *letter_iter_c = '\0';
+
+        rows.push_back((char**)malloc(3 * sizeof(char*))); // Allocate an index for 3 strings. Ech index represents a row
+        char** row = rows.at(index / 3);
+        *row = words.at(index); 
+        row++;
+        *row = words.at(index + 1);
+        row++;
+        *row = words.at(index + 2);
+        row++;
+
+        index = index + 3;
+        line_count++;
+    }
+    
+    //Inserts rows char** to structure char***
+    PerfRead.edges = (char***)malloc(rows.size() * sizeof(char**));
+    char*** edges_iter = PerfRead.edges;
+    for (int i = 0; i < rows.size(); i++)
+    {
+        *edges_iter = rows.at(i);
+        edges_iter++;
     }
 
-    /*
-      string idk = "asd";
-    
-    vector<char> test1(idk.begin(), idk.end());
-    char* test2 = test1.data();
+    //Section to insert all nodes in the set to the nodes**
+    PerfRead.nodes = (char**)malloc(node_set.size() * sizeof(char*));
+    words.clear();
+    char** nodes_iter = PerfRead.nodes;
+    index = 0;
+    for (auto it = node_set.begin(); it != node_set.end(); it++)
+    {
+        words.push_back((char*)malloc(25 * sizeof(char)));
+        char* letter_iter_a = words.at(index);
+        for (int i = 0; i < (*it).size(); i++)
+        {
+            *letter_iter_a = (*it).at(i);
+            letter_iter_a++;
+        }
+        (*letter_iter_a) = '\0';
+        *nodes_iter = words.at(index);
+        nodes_iter++;
+        index++;
+    }
 
-    vector<string> test3 = {"123", "1234"};
-    const char** test4 = nullptr;
-
-    for(int i = 0; i < test3.size(); i++){
-    
-      test4[i] = test3[i].data();
-    
-     }
-
-    */
-
-
-   /*
-      Alternate idea, run a for loop for evey char in the 3 strings for char*.
-      Then we appends those 3 strings to a char **(each column info).
-      Then the char ** is appended to a char ***(row).
-   */
-
+    //Commented code is to test if the edges*** correctly got all the strings. Worked with test data i used
+    /*auto help = PerfRead.edges;
+    int i = 0;
+    int k = 0;
+    while (i != line_count)
+    {
+        while (k != 3)
+        {
+            cout << **help << endl;
+            (*help)++;
+            k++;
+        }
+        k = 0;
+        help++;
+        i++;
+    }*/
 }
+
