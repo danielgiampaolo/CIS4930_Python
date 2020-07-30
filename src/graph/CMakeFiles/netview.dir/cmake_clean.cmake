e5f441a76fb6604cc3256f249421df7b068cb3c6
EnzoEguesDo <enzoegues@yahoo.com>
2020-07-30 14:44:43 -0400

    Just uploading new branch

---
+++ src/graph/CMakeFiles/netview.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/netview.dir/csv_parser.cpp.o"
+  "libnetview.pdb"
+  "libnetview.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/netview.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
