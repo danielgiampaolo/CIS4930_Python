d2e13c020381cbc2dff456bb8dd5d42a94a90a72
Enzo Egues <enzoegues@yahoo.com>
2020-08-01 21:01:16 -0400

    Updated csv parser to check if weight is not a number.

--- src/setup.py
+++ src/setup.py
@@ -20,7 +20,7 @@ c_lib = [Extension("libc_graph",
 
 setup(
     name = "NetView",
-    version = 2.0,
+    version = 2.2,
     packages= ["netview","sample","graph","templates"],
     package_data={'graph': ['libc_graph.so']},
     description='Network Grapher',
